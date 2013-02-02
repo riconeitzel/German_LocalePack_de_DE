2cac7de8a0d6412cd9c0bed9f6bd602febce96fb
Rico Neitzel <rico@buro71a.de>
2013-02-02 22:02:45 +0100

    Versionsnummer 1.7.0.2.4

--- app/code/community/German/LocalePackDe/Helper/Data.php
+++ app/code/community/German/LocalePackDe/Helper/Data.php
@@ -4,7 +4,7 @@
  * @package German_LocalePack
  * @authors Daniel Sasse <info@golox-web.de, http://www.golox-web.de/> - Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
  * @developer Daniel Sasse <info@golox-web.de, http://www.golox-web.de/> - Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
- * @version 1.7.0.2.2
+ * @version 1.7.0.2.4
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class German_LocalePackDe_Helper_Data extends Mage_Core_Helper_Abstract
