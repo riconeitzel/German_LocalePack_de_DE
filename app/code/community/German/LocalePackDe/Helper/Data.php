ca8400b78c26f1c8ba073d0ae569d99642ffc7c6
Rico Neitzel <rico@buro71a.de>
2014-05-30 21:19:37 +0200

    Version number in Module Files + Typo + 1.9 Notice for preinstalled crap translation

--- app/code/community/German/LocalePackDe/Helper/Data.php
+++ app/code/community/German/LocalePackDe/Helper/Data.php
@@ -4,7 +4,7 @@
  * @package German_LocalePack
  * @authors Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
  * @developer Rico Neitzel <rico@buro71a.de, http://www.buro71a.de/>
- * @version 1.8.0.0.2
+ * @version 1.9.0.1.0
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class German_LocalePackDe_Helper_Data extends Mage_Core_Helper_Abstract
