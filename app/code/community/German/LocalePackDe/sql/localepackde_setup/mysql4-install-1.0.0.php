c928f30bf7cb47063674c0934c4b15bb697f1bbf
MaWoScha <manfredwormschaefer@gmail.com>
2015-01-12 11:26:53 -0500

    Prevent exception while adding static block into DB.

--- app/code/community/German/LocalePackDe/sql/localepackde_setup/mysql4-install-1.0.0.php
+++ app/code/community/German/LocalePackDe/sql/localepackde_setup/mysql4-install-1.0.0.php
@@ -19,35 +19,43 @@ via <a title=\"Meine Service-Seite auf Skype\" href=\"skype:mein.laden?chat\" ta
 oder in Facebook auf unserer <a title=\"Meine Fanseite auf Facebook\" href=\"http://www.facebook.com/mein.laden\" target=\"_blank\">Meine Fanseite</a>.
 EOD;
 
-$cmsBlocks = array(
+$storeId = 1;
+
+$staticBlocks = array(
     array(
         'title'         => 'eMail Template Say Hello de',
         'identifier'    => 'email_template_say_hello',
         'content'       => 'Sehr geehrte(r)',
-        'is_active'     => 1,
-        'stores'        => 0
+        'is_active'     => 0,
+        'stores'        => array($storeId)
     ),
     array(
         'title'         => 'eMail Template Contact de',
         'identifier'    => 'email_template_contact',
         'content'       => $blockContent,
-        'is_active'     => 1,
-        'stores'        => 0
+        'is_active'     => 0,
+        'stores'        => array($storeId)
     ),
     array(
         'title'         => 'eMail Template Say Bye de',
         'identifier'    => 'email_template_say_bye',
         'content'       => 'Vielen Dank für Ihren Kauf!',
-        'is_active'     => 1,
-        'stores'        => 0
+        'is_active'     => 0,
+        'stores'        => array($storeId)
     )
 );
 
 /**
  * Insert default blocks
  */
-foreach ($cmsBlocks as $data) {
-    Mage::getModel('cms/block')->setData($data)->save();
+foreach ($staticBlocks as $data) {
+    try {
+        Mage::getModel('cms/block')->setData($data)->save();
+    } catch (Exception $e) {
+        # To prevent exception "A block identifier with the same properties already exists
+        # in the selected store." in "app:code:core:Mage:Cms:Model:Resource:Block.php"
+#        throw new Exception("Oops, mi error in DE German LocalePack");
+    }
 }
 
 $installer->endSetup();
