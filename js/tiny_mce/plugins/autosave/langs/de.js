1541d1e3fb92ebd18c91a66fdc9ac56a10f41765
MaWoScha <manfredwormschaefer@gmail.com>
2015-09-08 12:50:03 +0200

    'js/tiny_mce/plugins/autosave/langs/de.js' ergänzt

---
+++ js/tiny_mce/plugins/autosave/langs/de.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('en.autosave',{
+restore_content: "Wiederherstellung automatisch gespeicherter Inhalte",
+warning_message: "Wenn Sie die gespeicherten Inhalte wieder herstellen, werden Sie alle Inhalte, die sich derzeit in den Editor befinden, verlieren.\n\nSind Sie sicher, dass Sie die gespeicherte Inhalte wiederherstellen wollen?"
+});
